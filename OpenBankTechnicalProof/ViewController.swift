c52eca0d83e100a059ed3b08859d8787c9192d39
Sergio Castaño Panchuelo <>
2022-01-20 12:19:19 +0100

    Initial commit - Added project

---
+++ OpenBankTechnicalProof/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenBankTechnicalProof
+//
+//  Created by Sergio Castaño Panchuelo on 20/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OpenBankTechnicalProofUITests/OpenBankTechnicalProofUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  OpenBankTechnicalProofUITestsLaunchTests.swift
+//  OpenBankTechnicalProofUITests
+//
+//  Created by Sergio Castaño Panchuelo on 20/1/22.
+//
+
+import XCTest
+
+class OpenBankTechnicalProofUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
