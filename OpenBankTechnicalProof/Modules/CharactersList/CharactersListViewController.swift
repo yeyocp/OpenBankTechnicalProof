5aa378809d9d5fed5985dba4df17a80b76b28c3d
Sergio Castaño Panchuelo <>
2022-01-24 08:29:38 +0100

    Added CharactersListViewController

---
+++ OpenBankTechnicalProof/Modules/CharactersList/CharactersListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersListViewController.swift
+//  OpenBankTechnicalProof
+//
+//  Created by Sergio Castaño Panchuelo on 24/1/22.
+//
+
+import UIKit
+
+class CharactersListViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
