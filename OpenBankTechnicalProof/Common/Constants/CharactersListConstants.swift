d20bf9722eccd3cf6584a92c4d744756eddeb63f
Sergio Castaño Panchuelo <>
2022-01-24 12:15:52 +0100

    Added constants for services calls

--- OpenBankTechnicalProof/Common/Constants/CharactersListConstants.swift
+++ OpenBankTechnicalProof/Common/Constants/CharactersListConstants.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
+struct ServiceConstants {
+    static let marvelApiKey = "ba2d2dd75e334b3e306193b6a963de58"
+    
+    static let baseURL = "https://gateway.marvel.com:443"
+    static let charactersListPath = "/v1/public/characters?apikey=%@"
+    static let characterDetailPath = "/v1/public/characters/1011334?apikey=%@"
+}
+
 struct CharactersListConstants {
     static let characters = "characters.title".localized()
 }
