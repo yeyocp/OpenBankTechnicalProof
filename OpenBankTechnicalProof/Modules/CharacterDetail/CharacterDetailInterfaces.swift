3f6920ea3b4dc103ae38bb6a291f9d55db4e1654
Sergio Castaño Panchuelo <>
2022-01-24 18:21:03 +0100

    Added and implemented CharacterDetailInterfaces

---
+++ OpenBankTechnicalProof/Modules/CharacterDetail/CharacterDetailInterfaces.swift
@@ -0,0 +1,37 @@
+//
+//  CharacterDetailInterfaces.swift
+//  OpenBankTechnicalProof
+//
+//  Created by Sergio Castaño Panchuelo on 24/1/22.
+//
+
+import Foundation
+
+protocol CharacterDetailRouterInterface: RouterInterface {}
+
+protocol CharacterDetailViewInterface: ViewInterface {
+    var presenter: CharacterDetailViewDelegateInterface? { get set }
+    var characterDetail: CharacterDetailViewModel? { get }
+    
+    func didCharacterDetailFinish(_ characterDetailViewModel: CharacterDetailViewModel?)
+}
+
+protocol CharacterDetailViewDelegateInterface: PresenterInterface {
+    var router: CharacterDetailRouterInterface { get }
+    var view: CharacterDetailViewInterface { get }
+    var interactor: CharacterDetailInteractorInterface? { get }
+        
+    func fetchCharacterDetail()
+}
+
+protocol CharacterDetailInteractorInterface: InputInteractorInterface {
+    var presenter: CharacterDetailOutputInteractorInterface? { get set }
+    var charactersManagementService: CharactersManagementService { get }
+    
+    func fetchCharacterDetail()
+}
+
+protocol CharacterDetailOutputInteractorInterface: OutputInteractorInterface {
+    func onCharacterDetailSucceed(characterDetailDomain: CharacterItemDomainModel)
+    func onCharactersDetailFailed()
+}
